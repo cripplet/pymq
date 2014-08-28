94b38038fb33b674d5132712c06f274e96942169
Minke Zhang <minke.zhang@gmail.com>
2014-08-28 15:17:34 -0500

    added settings

--- pymq/classes/item.py
+++ pymq/classes/item.py
@@ -1,2 +1,21 @@
+from pymq.settings import TMP_DIR
+
 class MessageItem(object):
-	pass
+	def __init__(self, *args, **kwargs):
+		self.eid = None
+
+		self.dat = None
+		self.obj = None
+
+		self.init_time = None
+		self.drop_time = None
+
+		self._filename = None
+
+		super(MessageItem).__init__(*args, **kwargs)
+
+	@property
+	def filename(self):
+		if(self._filename == None):
+			self._filename = '%s/%s' % (TMP_DIR, self.eid)
+		return(self._filename)
---
+++ pymq/settings/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+TMP_DIR = '/tmp/pymq'
+
+if not os.path.exists(TMP_DIR):
+	os.makedirs(TMP_DIR)
